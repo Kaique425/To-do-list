e9af9a5b3776e45ece7db11e4c796ff4b59a931b
Kaique <motamendes.kaique.100@gmail.com>
2021-06-26 13:05:58 -0300

    Adicionando a tela de login de usuarios

--- Tarefas/urls.py
+++ Tarefas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path 
-from .views import TarefasDetail, TarefasList, TarefasCreate, TarefasUpdate, TarefasDelete
+from .views import TarefasDetail, TarefasList, TarefasCreate, TarefasUpdate, TarefasDelete, UsuarioLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('logout',LogoutView.as_view(next_page='login'), name='logout' ),
+    path('login',UsuarioLoginView.as_view(), name='login'),
     path('', TarefasList.as_view(), name='listatarefa'),
     path('tarefa/<int:pk>',TarefasDetail.as_view(),name='detalhetarefa'),
     path('criar-tarefa', TarefasCreate.as_view(), name='criartarefa'),
--- Tarefas/views.py
+++ Tarefas/views.py
@@ -1,34 +1,57 @@
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from  .models import Tarefas
 from django.urls import reverse_lazy
+from  .models import Tarefas
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class TarefasList(ListView):
+
+class UsuarioLoginView(LoginView):
+    template_name='Tarefas/login.html'
+    fields = ['titulo', 'descricao', 'completa']
+    redirect_authenticated_user = True 
+
+    
+    def get_success_url(self):
+        return reverse_lazy('listatarefa')
+
+class TarefasList(LoginRequiredMixin,ListView):
     model = Tarefas
     context_object_name = 'Tarefas'
-    template_name= 'Tarefas/tarefas_list.html'
+    template_name = 'Tarefas/tarefas_list.html'
+
+    #Filtrar pro usuario
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Tarefas'] = context['Tarefas'].filter(user=self.request.user)
+        context['Count'] = context['Tarefas'].filter(completa=False).count()
+        return context
 
 
-class TarefasDetail(DetailView):
+class TarefasDetail(LoginRequiredMixin,DetailView):
     model = Tarefas
     context_object_name = 'Tarefas'
 
 
-class TarefasCreate(CreateView):
+class TarefasCreate(LoginRequiredMixin,CreateView):
     model = Tarefas
-    fields = '__all__'
-    success_url = reverse_lazy('listatarefa')
-
-class TarefasUpdate(UpdateView):
-    model = Tarefas
-    fields = '__all__'
+    fields = ['titulo', 'descricao', 'completa']
     success_url = reverse_lazy('listatarefa')
 
 
-class TarefasDelete(DeleteView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TarefasCreate, self).form_valid(form)
+
+class TarefasUpdate(LoginRequiredMixin,UpdateView):
+    model = Tarefas
+    fields = ['titulo', 'descricao', 'completa']
+    success_url = reverse_lazy('listatarefa')
+
+
+class TarefasDelete(LoginRequiredMixin,DeleteView):
     model = Tarefas
     success_url = reverse_lazy('listatarefa')
     context_object_name = 'Tarefas'
